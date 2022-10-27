fdffee681c98f36a148feda66e94f58b9b06b342
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-27 15:14:53 +0300

    Update loadingBtn.js

--- src/js/loadingBtn.js
+++ src/js/loadingBtn.js
@@ -1,23 +1,23 @@
-export { onScroll, onToTopBtn };
+export { onScroll, onScrollToTopBtn };
 
-const toTopBtn = document.querySelector('.btn-to-top');
+const btnToTop = document.querySelector('.btn-to-top');
 
 window.addEventListener('scroll', onScroll);
-toTopBtn.addEventListener('click', onToTopBtn);
+btnToTop.addEventListener('click', onScrollToTopBtn);
 
 function onScroll() {
   const scrolled = window.pageYOffset;
   const coords = document.documentElement.clientHeight;
 
   if (scrolled > coords) {
-    toTopBtn.classList.add('btn-to-top--visible');
+    btnToTop.classList.add('btn-to-top--visible');
   }
   if (scrolled < coords) {
-    toTopBtn.classList.remove('btn-to-top--visible');
+    btnToTop.classList.remove('btn-to-top--visible');
   }
 }
 
-function onToTopBtn() {
+function onScrollToTopBtn() {
   if (window.pageYOffset > 0) {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
