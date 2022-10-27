a2347a953b1fff250a561971a0ae9ae99051fb50
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-27 15:47:02 +0300

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -4,8 +4,6 @@ export { fetchImages };
 const API_KEY = '29966303-d9893c98832d118b1f4c04955';
 const baseUrl = 'https://pixabay.com/api/';
 
-const axios = require('axios').default;
-
 async function fetchImages(searchImgName, page) {
   return await axios({
     method: 'get',
